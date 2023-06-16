8046edb38df8a29f3bec6802aa3a59c19940d81c
pluto096 <oyewopelumi99@gmail.com>
2023-06-16 13:53:55 -0700

    furi

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int n;
+    int ld;
+
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    ld = n % 10;
+
+    return (0);
+}
