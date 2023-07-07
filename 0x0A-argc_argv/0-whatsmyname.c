4a29492fe4cad18f7f3758aab54536a7fad7bba7
pluto096 <oyewopelumi99@gmail.com>
2023-07-07 22:59:39 +0100

    task 1 completed

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints its name
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Program that prints number of arguments
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
