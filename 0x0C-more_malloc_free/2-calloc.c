bed2c7ff39ec9b5b3de89c3a8142b6caa37b7c19
pluto096 <oyewopelumi99@gmail.com>
2023-07-13 22:43:42 +0100

    removed sting.h lib>

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,24 @@
-#include<string.h>
 #include<stdlib.h>
 #include "main.h"
+
+/**
+ * _memset - fills memory with a constant
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ *
+ * Return: Pointer to memory area s
+ */
+char _memset(char *s, char *b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
 
 /**
  * _calloc - allocates memory of an array
@@ -20,6 +38,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	if (clloc == NULL)
 		return (NULL);
-	memset(clloc, 0, nmemb * size);
+	_memset(clloc, 0, nmemb * size);
 	return (clloc);
 }
