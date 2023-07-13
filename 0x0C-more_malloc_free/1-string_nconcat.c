8898fe3b6d1fb19c60c767f9ae97b3520b30c2e4
pluto096 <oyewopelumi99@gmail.com>
2023-07-13 16:05:01 +0100

    indented with btty style

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,8 +4,8 @@
 
 /**
  * string_nconcat - concatenates two strings
- * @*s1: first string to be joined
- * @*s2: second string to be joined
+ * @s1: first string to be joined
+ * @s2: second string to be joined
  * @n: length of s2 to be appended on s1
  *
  * Return: Pointer of new memory allocated or NULL if allocation fails
@@ -15,12 +15,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s;
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	while(s1 && s1[len1])
+	while (s1 && s1[len1])
 		len1++;
-	while(s2 && s2[len2])
+	while (s2 && s2[len2])
 		len2++;
 
-	if(n < len2)
+	if (n < len2)
 	{
 		s = malloc(sizeof(char) * (len1 + n + 1));
 	}
@@ -29,16 +29,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s = malloc(sizeof(char) * (len1 + len2 + 1));
 	}
 
-	if(!s)
+	if (!s)
 		return (NULL);
-	while(i < len1)
+	while (i < len1)
 	{
 		s[i] = s1[i];
 		i++;
 	}
-	while(n < len2 && i < (len1 + n))
+	while (n < len2 && i < (len1 + n))
 		s[i++] = s2[j++];
-	while(n >= len2&& i < (len1 + len2))
+	while (n >= len2 && i < (len1 + len2))
 		s[i++] = s2[j++];
 	s[i] = '\0';
 
