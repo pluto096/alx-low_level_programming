52097df5c3517b4a8080a638982a3327aaa7bc36
pluto096 <oyewopelumi99@gmail.com>
2023-07-18 01:24:53 +0100

    second commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,10 +1,10 @@
-#include <stdlib.h>
+#include<stdlib.h>
 #include "dog.h"
 
 /**
- * init_dog - initializes a variable of type struct dog
- * @d: pointer to struct dog to initialize
- * @name: name to initialize
+ * init_dog - initialize a variableof struct dog
+ * @d: pointer to struct dog
+ * @name: name to initalize
  * @age: age to initialize
  * @owner: owner to initialize
  */
